32d50ca128a8292dd244d7f9fd51f96a0393a9a4
josh <sandbox7226@gmail.com>
2025-05-22 09:59:32 -0400

    Create partB.java

---
+++ Q3/partB.java
@@ -0,0 +1,19 @@
+public ArrayList<Match> buildMatches()
+{
+    ArrayList<Match> matches = new ArrayList<Match>();
+
+    int bestIndex = competitorList.size() % 2;
+    int worstIndex = competitorList.size() - 1;
+
+    while(bestIndex < worstIndex)
+    {
+        matches.add(new Match(
+                competitorList.get(bestIndex),
+                competitorList.get(worstIndex)));
+
+        bestIndex++;
+        worstIndex--;
+    }
+
+    return matches;
+}
