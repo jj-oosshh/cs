9372e47e3d61914e892267f8ce9e0f9f282854da
josh <sandbox7226@gmail.com>
2025-05-22 09:49:51 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,1 +1,11 @@
+//part A
 
+public int walkDogs(int hour) {
+    int numDogs = company.numAvailableDogs(hour);
+       if (maxDogs >= numDogs) {
+        company.updateDogs(hour, numDogs);
+        return numDogs;
+    } 
+    company.updateDogs(hour, maxDogs);
+    return maxDogs;
+}
